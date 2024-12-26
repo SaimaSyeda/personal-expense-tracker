53dea9d300532ef53f13817f50fabb586cde3eb2
SaimaSyeda <saimafatima417@gmail.com>
2024-12-26 13:06:12 +0530

    Initial commit

---
+++ expense-tracker-app/src/test/java/com/saima/expense/ExpenseTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saima.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
