60d7b662e20ca3f4d4611cc9e12196f6aa49806d
SaimaSyeda <saimafatima417@gmail.com>
2024-12-29 11:33:33 +0530

    Rest APIs created for CRUD operations

--- expense-tracker-app/src/main/java/com/saima/expense/service/CategoryService.java
+++ expense-tracker-app/src/main/java/com/saima/expense/service/CategoryService.java
@@ -2,6 +2,16 @@ package com.saima.expense.service;
 
 import com.saima.expense.dto.CategoryDto;
 
+import java.util.List;
+
 public interface CategoryService {
     CategoryDto createCategory(CategoryDto categoryDto);
+
+    CategoryDto getCategoryById(Long categoryId);
+
+    List<CategoryDto> getAllCategories();
+
+    CategoryDto updateCategory(Long categoryId, CategoryDto categoryDto);
+
+    void deleteCategory(Long categoryId);
 }
--- expense-tracker-app/src/main/java/com/saima/expense/service/impl/CategoryServiceImpl.java
+++ expense-tracker-app/src/main/java/com/saima/expense/service/impl/CategoryServiceImpl.java
@@ -5,8 +5,13 @@ import com.saima.expense.entity.Category;
 import com.saima.expense.mapper.CategoryMapper;
 import com.saima.expense.repository.CategoryRepository;
 import com.saima.expense.service.CategoryService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
@@ -22,5 +27,42 @@ public class CategoryServiceImpl implements CategoryService {
 
         //convert Entity to Dto
         return CategoryMapper.mapToCategoryDto(savedCategory);
+    }
+
+    @Override
+    public CategoryDto getCategoryById(Long categoryId) {
+
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found with id: " + categoryId));
+        return CategoryMapper.mapToCategoryDto(category);
+    }
+
+    @Override
+    public List<CategoryDto> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+
+        return categories.stream()
+                .map((category) -> CategoryMapper.mapToCategoryDto(category))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDto updateCategory(Long categoryId, CategoryDto categoryDto) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found with id: " + categoryId));
+
+        category.setName(categoryDto.name());
+
+        Category updatedCategory = categoryRepository.save(category);
+
+        return CategoryMapper.mapToCategoryDto(updatedCategory);
+    }
+
+    @Override
+    public void deleteCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found with id: " + categoryId));
+
+        categoryRepository.delete(category);
     }
 }
